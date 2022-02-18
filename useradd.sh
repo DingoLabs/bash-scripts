db55ebe9da640043ed83bf0e3392555e010bb1d4
D1ngo <63161644+DingoLabs@users.noreply.github.com>
2022-02-18 09:13:43 -0500

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-echo 'enter new user name: '
-read NEW_USER
-echo 'enter password to elevate to root'
-read -s MY_PASS
-echo "creating new user ${NEW_USER} now"
-echo ${MY_PASS} | sudo -S useradd -m  ${NEW_USER}
+
+#create an account on system, prompts for username password
+
+#ask username
+read -p "enter username: " USER_NAME
+#ask real name
+read -p "enter full name: " COMMENT
+#ask for password
+read -p "enter password: " PASSWORD
+#create user
+useradd -c "${COMMENT}" -m ${USER_NAME}
+#set password
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+#force password update on login
+passwd -e ${USER_NAME}
